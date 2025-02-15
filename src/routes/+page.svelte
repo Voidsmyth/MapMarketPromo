dd74f5dd39dec43f5d911cff59bad9767503d1df
Kobe Edmond <e.kobedevon@gmail.com>
2025-02-15 08:48:34 +0000

    initial site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class=" flex justify-center">
+    <div class="prose prose-slate w-full px-20 py-12 flex flex-col items-center">
+	<h1>The Map Manifesto Draft</h1>
+	<p>The current system for TTRPG map is fundamentaly broken.</p>
+	<ul>
+		<li>No good way for users or creators to access all their maps and subscriptions </li>
+		<li>Platforms take large percentages from creators while providing mediocre support</li>
+		<li>No easy way for creators to protect their libraries</li>
+	</ul>
+    <h2>
+        What is this nonsense about
+    </h2>
+	<p>
+		This page servers as a first draft for what the website will be about. However it also serves the purpose of learning to dockerize svelte. If you are seeing this I have not figured it out yet.
+	</p>
+    <p>
+        Back in summer 2021 I had an idea of a site actually for artists. Inspired by drivers.coop in NYC, a mission to make a place where the goal is to keep as much of the money in creators pockets.
+        A goal to target sustainability and longterm thinking without shareholders and such. 
+    </p>
+    <p>
+        Focused on building a proper lasting ecosytem. A more symbiotic relationship between the site and the creators as a site is nothing without them. So why try to piss them off?
+    </p>
+    <h2> What effort is being made </h2>
+    <p> A protoype has been made, a map serving website you could subscribe to creators and manage a easy shared library of all your favorites.
+        A improved version is being developed and I plan to release more detail soon. This site serves as a test of setting up CI/CD for a site. 
+    </p>
+    <p>
+        This site will be using a fully opensource stack that is host agnositc in order to minimize cost. Taking the effort to build sound systems that are affordable yet reliable. 
+        I understand these are lofty claims and I ask nothing from you,I mearly hope my actions prove I truly intend to honor my words. Thank you for reading.
+    </p>
+    <h3>Interested in following the project?</h3>
+    <p>
+        Sign up for updates <a href="https://forms.gle/9AJvDxCWhTJ46gyb8">here.</a> I Promise not to spam you.
+    </p>
+</div>
+</div>
